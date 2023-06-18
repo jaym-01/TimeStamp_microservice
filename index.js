2402052fe573962d883b163474c4eaa11145bf6f
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-06-18 19:16:28 +0100

    attempt 1

--- index.js
+++ index.js
@@ -13,6 +13,34 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/extra/main.html");
 });
 
+app.get('/api/:date?', (req, res) => {
+    try{
+        let res_date;
+
+        if(req.params.date == null){
+            res_date = new Date();
+            res.json({"unix": Date.now(), "utc": res_date.toUTCString()});
+        }
+        else{
+            res_date = new Date(req.params.date);
+            if(res_date.toUTCString() == "Invalid Date"){
+                res_date = new Date(req.params.date * 1);
+                res.json({"unix": res_date.valueOf(), "utc": res_date.toUTCString()});
+
+                if(res_date.toUTCString() == "Invalid Date"){
+                    res.json({"error":"Invalid Date"});
+                }
+            }
+            res.json({"unix": Date.parse(req.params.date), "utc": res_date.toUTCString()});
+        }
+
+        
+    }
+    catch(err){
+        res.json({"error":"Invalid Date"})
+    }
+});
+
 var listener = app.listen(process.env.PORT, function(){
     console.log("listening on port " + process.env.PORT + "...");
 });
